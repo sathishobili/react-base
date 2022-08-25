7accf573eb584dd3b8d1c3db00ff2a70991ce82f
sathish obili <sathishop@cardinality.ai>
2022-08-25 22:21:09 +0530

    added store and getting list from store

---
+++ src/core/state/user/usersSlice.tsx
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: "1", name: "Dave Patrick", email: "dave@gmail.com" },
+  { id: "2", name: "Hank Gluhwein", email: "hank@gmail.com" },
+];
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    userAdded(state, action) {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { userAdded } = usersSlice.actions;
+
+export default usersSlice.reducer;
--- src/pages/dashboard/user/list/index.tsx
+++ src/pages/dashboard/user/list/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { useAppSelector } from "../../../../core/state/hooks";
 
 const UserList = () => {
+  const users = useAppSelector((state) => state.users);
   return (
     <div>
       UserList
-      <Outlet />
+      <table>
+        <tbody>
+          {users.map(({ id, name, email }, i) => (
+            <tr key={i}>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{email}</td>
+              <td>
+                <button>Delete</button>
+                <button>Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
